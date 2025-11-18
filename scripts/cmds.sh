15f2370ddf28d57e8492a6fcdda6c4ed4ae216cf
ligf <ligf@mingyuanyun.com>
2025-11-18 19:18:20 +0800

    优化测试命令

--- scripts/cmds.sh
+++ scripts/cmds.sh
@@ -1,25 +1,14 @@
-# 完整启动
-npx tsx src/cli.ts --serve --port 3001 --project /Users/ligf/Code/claude-code/ccdebug/ccdemo --include-all-requests
+# 启动web（ccdemo）
+npx tsx src/cli.ts --serve --port 4101 --project ccdemo
 
-# 仅启动web
-npx tsx src/cli.ts --serve --port 3008 --project /Users/ligf/Code/claude-code/ccdebug/ccdemo
+# 启动web（其他目录）
+npx tsx src/cli.ts --serve --port 4102 --project /Users/ligf/工作/Code/平台/ccmodeling/platform_ai/0011
+npx tsx src/cli.ts --serve --port 4103 --project /Users/ligf/工作/Code/平台/ccmodeling/platform_ai/x_0338
 
-npx tsx src/cli.ts --serve --port 3001 --project /Users/ligf/工作/Code/平台/ccmodeling/platform_ai/0011
+# 启动cc（快速测试）
+npx --node-options="--inspect-brk=9321" tsx src/cli.ts  --run-with -p "1+2=?" --output-format stream-json --verbose
 
-npx tsx src/cli.ts --serve --port 3008 --project /Users/ligf/工作/Code/平台/ccmodeling/platform_ai/x_0338
+npx --inspect-brk tsx src/cli.ts  --run-with -p "1+2=?" --output-format stream-json --verbose
 
-
-npx tsx ../src/cli.ts
-
-npx tsx src/cli.ts --serve --port 3007
-
-
-cat 3.图书管理应用.md | npx tsx ../src/cli.ts --run-with -p  --output-format stream-json --verbose
-
-cat docs/test.md | npx tsx ../src/cli.ts --run-with -p --output-format stream-json --verbose
-
- npx tsx src/cli.ts --claude-path "C:\Users\ligf\AppData\Roaming\npm\node_modules\@anthropic-ai\claude-code\cli.js"  --run-with -p "1+2=?" --output-format stream-json --verbose
-
-npx tsx src/cli.ts  --run-with -p "1+2=?" --output-format stream-json --verbose
-
-# 主题：如何设计Agent
+# 启动cc（完整测试）
+cd ccdemo && cat ../scripts/test.md | npx tsx ../src/cli.ts --run-with -p  --output-format stream-json --verbose
